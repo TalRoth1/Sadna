d49019423919502c24f61dde904eb242614460fa
Tal <rothta@post.bgu.ac.il>
2026-04-28 12:48:03 +0300

    added testing classes

---
+++ Version1/src/test/java/org/example/ApplicationLayer/NotificationServiceTest.java
@@ -0,0 +1,12 @@
+package org.example.ApplicationLayer;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class NotificationServiceTest {
+
+    @Test
+    public void testAddUser() {
+        assertTrue(true);
+    }
+}
---
+++ Version1/src/test/java/org/example/ApplicationLayer/UserServiceTest.java
@@ -0,0 +1,12 @@
+package org.example.ApplicationLayer; // Double check the spelling here!
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class UserServiceTest {
+
+    @Test
+    public void testAddUser() {
+        assertTrue(true);
+    }
+}
