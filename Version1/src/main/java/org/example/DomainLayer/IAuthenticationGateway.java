84e73b41da44e33c91ce877296d5da031c46a3eb
sabika19971 <sabika@post.bgu.ac.il>
2026-04-30 11:51:15 +0300

    validation of added fields for user and writing messages in English

--- Version1/src/main/java/org/example/DomainLayer/IAuthenticationGateway.java
+++ Version1/src/main/java/org/example/DomainLayer/IAuthenticationGateway.java
@@ -3,5 +3,8 @@ package org.example.DomainLayer;
 public interface IAuthenticationGateway {
     String hashPassword(String plainPassword); // מיוז קייס 2
     boolean verifyPassword(String plainPassword, String hashedPassword); // מיוז קייס 3
+    boolean verifyEmail(String email);
+    boolean verifyPassword(String pass);
+    boolean verifyUserDetails(String email, String password, float age, String username);
 
 }
