045de2e41777c90b6157e129f004c089214f137a
NaamaShlittner <shlittna@post.bgu.ac.il>
2026-05-03 00:11:19 +0300

    added lottery repository

--- Version1/src/main/java/org/example/DomainLayer/PolicyAggregate/PurchasePolicy.java
+++ Version1/src/main/java/org/example/DomainLayer/PolicyAggregate/PurchasePolicy.java
@@ -1,5 +1,30 @@
 package org.example.DomainLayer.PolicyAggregate;
+import org.example.DomainLayer.DomainException;
 
 public class PurchasePolicy {
-    public PurchasePolicy(){}
+    private int minTicketsPerPurchase;
+    private int maxTicketsPerPurchase;
+    private int minimumAge;
+
+    public PurchasePolicy() {
+        this.minTicketsPerPurchase = 1;
+        this.maxTicketsPerPurchase = Integer.MAX_VALUE;
+        this.minimumAge = 0;
+    }
+
+    public void validateTicketAmount(int amount) {
+        if (amount < minTicketsPerPurchase) {
+            throw new DomainException("amount of tickets is lower than the minimum required");
+        }
+
+        if (amount > maxTicketsPerPurchase) {
+            throw new DomainException("amount of tickets is higher than the maximum allowed");
+        }
+    }
+
+    public void validateMemberAge(int age) {
+        if (age < minimumAge) {
+            throw new DomainException("age of the member is not suitable for the policy");
+        }
+    }
 }
