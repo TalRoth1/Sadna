fee24060a378525c6f80f565c98d47a5a21b3f32
Tal <rothta@post.bgu.ac.il>
2026-05-13 00:37:58 +0300

    fixed company test mockito

--- Version1/src/test/java/org/example/DomainLayer/CompanyAggregate/CompanyTest.java
+++ Version1/src/test/java/org/example/DomainLayer/CompanyAggregate/CompanyTest.java
@@ -1,9 +1,5 @@
 package org.example.DomainLayer.CompanyAggregate;
 
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
-
-@RunWith(MockitoJUnitRunner.class)
 public class CompanyTest {
     
 }
