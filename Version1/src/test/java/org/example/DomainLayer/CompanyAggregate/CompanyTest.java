bc67562ac4284cc0061c4f8da06a0dfd916d61a2
Tal <rothta@post.bgu.ac.il>
2026-05-13 00:35:03 +0300

    added logs for company creation

---
+++ Version1/src/test/java/org/example/DomainLayer/CompanyAggregate/CompanyTest.java
@@ -0,0 +1,9 @@
+package org.example.DomainLayer.CompanyAggregate;
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CompanyTest {
+    
+}
