c11f0567b73067032ac3874c69f3b7da8e67a05e
NadavSvilim <nadavsvi@post.bgu.ac.il>
2026-05-14 10:49:04 +0300

    added base for UI project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="app-root">
+      <h1>Frontend for Sadna</h1>
+      <p>This is a basic page created inside the frontend project.</p>
+    </main>
+  )
+}
+
+export default App
