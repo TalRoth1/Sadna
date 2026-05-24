8b8d5da6aa75a28a72397768f578c6e379abec3a
levido454 <idou@post.bgu.ac.il>
2026-05-24 12:53:24 +0300

    Maximum ticket amount for lottery is now enforced

--- Version1/src/main/java/org/example/DomainLayer/PolicyManagment/PurchasePolicy.java
+++ Version1/src/main/java/org/example/DomainLayer/PolicyManagment/PurchasePolicy.java
@@ -15,27 +15,22 @@ public class PurchasePolicy {
         return rules;
     }
 
-    public void addRule(IPurchaseRule rule, boolean andOr)
-    {
+    public void addRule(IPurchaseRule rule, boolean andOr) {
         if (this.rules == null)
             this.rules = rule;
         else
             this.rules = new PurchaseComposite(rules, rule, andOr);
     }
 
-    public void removeRule(UUID ruleId)
-    {
-        if (ruleId == rules.getId())
-            {
-                rules = null;
-            }
-        else if (rules instanceof PurchaseComposite)
-            rules = ((PurchaseComposite)rules).removeRule(ruleId);
+    public void removeRule(UUID ruleId) {
+        if (ruleId == rules.getId()) {
+            rules = null;
+        } else if (rules instanceof PurchaseComposite)
+            rules = ((PurchaseComposite) rules).removeRule(ruleId);
     }
 
-    public boolean validate(ActivePurchase purchase, User user, Event event)
-    {
-        if(this.rules == null)
+    public boolean validate(ActivePurchase purchase, User user, Event event) {
+        if (this.rules == null)
             return true;
         return rules.doesHold(purchase, user, event);
     }
